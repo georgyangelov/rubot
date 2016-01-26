b838081913af43457b87a7fff2fa56afe5021806
Georgy Angelov <georgyangelov@gmail.com>
2016-01-27 01:28:32 +0200

    Add `how are you` responses

--- lib/rubot/commands/ping_pong.rb
+++ lib/rubot/commands/ping_pong.rb
@@ -14,8 +14,19 @@ module Rubot
         client.say channel: data.channel, text: HELLO_RESPONSES.sample
       end
 
-      command(/к'?во става/) do |client, data, _|
+      command(/(к'?во става|какво става)/) do |client, data, _|
         client.say channel: data.channel, text: 'К\'во да става бе човек...'
+      end
+
+      HOW_ARE_YOU_RESPONSES = [
+        'Аз съм бот бе човек, как може да съм.',
+        'Горе-долу...',
+        'Бивам',
+        'Добре съм, ям рам и спя по цял ден. Райски живот си живея направо.',
+      ].deep_freeze
+
+      command(/как си/) do |client, data, _|
+        client.say channel: data.channel, text: HOW_ARE_YOU_RESPONSES.sample
       end
 
       INTRODUCE_YOURSELF_RESPONSE = 'Здравейте ^_^ Аз съм Рубот. Отговарям на всякакви ' \
