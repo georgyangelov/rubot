f4e1054266c454845536c381d319739db86f93a3
Georgy Angelov <georgyangelov@gmail.com>
2016-01-27 01:40:28 +0200

    Add a few more responses

--- lib/rubot/commands/ping_pong.rb
+++ lib/rubot/commands/ping_pong.rb
@@ -14,7 +14,7 @@ module Rubot
         client.say channel: data.channel, text: HELLO_RESPONSES.sample
       end
 
-      command(/(к'?во става|какво става)/) do |client, data, _|
+      command(/(к'?во|какво) става/) do |client, data, _|
         client.say channel: data.channel, text: 'К\'во да става бе човек...'
       end
 
@@ -23,10 +23,26 @@ module Rubot
         'Горе-долу...',
         'Бивам',
         'Добре съм, ям рам и спя по цял ден. Райски живот си живея направо.',
+        'А ти нямаш ли си работа?',
       ].deep_freeze
 
       command(/как си/) do |client, data, _|
         client.say channel: data.channel, text: HOW_ARE_YOU_RESPONSES.sample
+      end
+
+      WHAT_ARE_YOU_DOING_RESPONSES = [
+        'Аз съм бот бе човек, какво може да правя.',
+        'Бича айляк',
+        'Ям рам. Остави ме намира',
+        'Въртя си харда за кеф',
+        'Пържа си яйца на процесора',
+        'Гледам клипчета в YouTube',
+        'А ти нямаш ли си работа?',
+        'Чакам някой да се сети да ми даде работа',
+      ].deep_freeze
+
+      command(/(к'?во|какво) правиш/) do |client, data, _|
+        client.say channel: data.channel, text: WHAT_ARE_YOU_DOING_RESPONSES.sample
       end
 
       INTRODUCE_YOURSELF_RESPONSE = 'Здравейте ^_^ Аз съм Рубот. Отговарям на всякакви ' \
--- lib/rubot/commands/unknown.rb
+++ lib/rubot/commands/unknown.rb
@@ -3,7 +3,6 @@ module Rubot
     class Unknown < Rubot::Command
       NO_COMMAND = /\s*/.freeze
       ARE_YOU_HERE_COMMAND = /(тука? ли си|тук\?)/.freeze
-      PUNCTUATION_COMMAND = /(?<command>[\s\.!@?]*)/.freeze
       UNKNOWN_COMMAND = /(?<command>.*)/.freeze
 
       NO_COMMAND_RESPONSES = [
@@ -17,17 +16,13 @@ module Rubot
         'ZZZzzzzzz... А? Кой смее да ме буди от вечния ми сън?',
       ].deep_freeze
 
-      PUNCTUATION_COMMAND_RESPONSES = [
-        'Не го разбирам това твоето `%s`',
-        'Не мога да те разбера. Това `%s` на какъв език е?',
-        'Какви са тези символи дето ми пращаш? `%s` ще ми вика...',
-      ].deep_freeze
-
       UNKNOWN_COMMAND_RESPONSES = [
         'Какво трябва да значи `%s`?',
         'Не го разбирам това твоето `%s`',
+        'Не мога да те разбера. Това `%s` на какъв език е?',
         'Не мога да те разбера, честно. Какво е `%s`?',
         'Извинявай, но не мога да парсна `%s` :(',
+        'Какви са тези неща дето ми ги пращаш? `%s` ще ми вика...',
         'Уважаеми господине/госпожо. Моля Ви да извините невежеството ми, но аз съм само един ' \
           'беден бот и не разбирам какво ми казвате. `%s` за мен не означава нищо.',
       ].deep_freeze
@@ -40,11 +35,6 @@ module Rubot
       command ARE_YOU_HERE_COMMAND do |client, data, _|
         client.say channel: data.channel,
                    text: NO_COMMAND_RESPONSES.sample
-      end
-
-      command PUNCTUATION_COMMAND do |client, data, match|
-        client.say channel: data.channel,
-                   text: PUNCTUATION_COMMAND_RESPONSES.sample % match[:command]
       end
 
       command UNKNOWN_COMMAND do |client, data, match|
