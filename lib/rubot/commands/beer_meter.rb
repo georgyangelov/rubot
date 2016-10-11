3d812ee6b75bf52a41456da746cbe309bc31a3cf
Georgy Angelov <georgyangelov@gmail.com>
2016-10-11 19:02:23 +0300

    Add vodka meter

--- lib/rubot/commands/beer_meter.rb
+++ lib/rubot/commands/beer_meter.rb
@@ -2,12 +2,20 @@ module Rubot
   module Commands
     class BeerMeter < Rubot::Command
       BEER_STATUS_COMMAND = /има ли бир(а|и)( в хладилника)?/i
+      VODKA_STATUS_COMMAND = /има ли (водка|кола|мента|(твърд )?алкохол)( в хладилника)?/i
 
       BEER_SCALE = {
-        'Няма бира. Ходи да купиш.'    => 400,
-        'Има малко бира. Граби бързо.' => 900,
-        'Има бира.'                    => 915,
+        'Няма бира. Ходи да купиш.'    => 150,
+        'Има малко бира. Граби бързо.' => 800,
+        'Има бира.'                    => 910,
         'Има много бира.'              => 2000,
+      }.freeze
+
+      VODKA_SCALE = {
+        'Няма водка.'                     => 150,
+        'Има малко водка. Крийте я!'      => 600,
+        'Има достатъчно водка.'           => 850,
+        'Някой е сложил диня на сензора!' => 2000,
       }.freeze
 
       desc 'има ли бира', 'Казва дали има бира'
@@ -16,23 +24,32 @@ module Rubot
 
         puts "Beer status request from #{user.profile.email}"
 
-        send_beer_response client, data
+        send_response client, data, 'left', BEER_SCALE
       end
 
-      def self.send_beer_response(client, data)
+      desc 'има ли водка', 'Казва дали има водка'
+      command VODKA_STATUS_COMMAND do |client, data, _|
+        user = client.users[data.user]
+
+        puts "Vodka status request from #{user.profile.email}"
+
+        send_response client, data, 'right', VODKA_SCALE
+      end
+
+      def self.send_response(client, data, sensor, scale)
         sensors  = HttpRequests.get_json("#{Rubot::SECRETS['beer_mcu_url']}/status")
-        response = beer_amount_for_sensor_value(sensors['left'], sensors['right'])
+        response = response_for_sensor_value(scale, sensors[sensor])
 
         client.say channel: data.channel, text: response
       rescue HttpRequests::RemoteError => error
-        puts 'Error getting beer sensor values'
+        puts 'Error getting sensor values'
         p error
 
         client.say channel: data.channel, text: 'Няма връзка с хладилника!'
       end
 
-      def self.beer_amount_for_sensor_value(left_value, _right_value)
-        BEER_SCALE.find { |response, value| left_value <= value }.first
+      def self.response_for_sensor_value(scale, reading)
+        scale.find { |response, value| reading <= value }.first
       end
     end
   end
